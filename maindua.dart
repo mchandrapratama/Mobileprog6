cf146059d83d04287bf6eb3c4d851396ac7e0fd5
mchandrapratama <144599226+mchandrapratama@users.noreply.github.com>
2023-10-06 20:30:43 +0700

    Add files via upload

---
+++ maindua.dart
@@ -0,0 +1,64 @@
+ // ignore_for_file: deprecated_member_use, library_private_types_in_public_api
+
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: AccessCameraPage(),
+    );
+  }
+}
+
+class AccessCameraPage extends StatefulWidget {
+  const AccessCameraPage({super.key});
+
+  @override
+  _AccessCameraPageState createState() => _AccessCameraPageState();
+}
+
+class _AccessCameraPageState extends State<AccessCameraPage> {
+  File? _image;
+
+  Future<void> openCamera() async {
+    final pickedImage =
+        await ImagePicker().getImage(source: ImageSource.camera);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Access Camera"),
+      ),
+      body: Center(
+        child: _image == null ? const Text("No Image") : Image.file(_image!),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: () {
+          openCamera();
+        },
+        child: const Icon(
+          Icons.add_a_photo,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
